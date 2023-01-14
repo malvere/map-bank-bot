4b5fe1dbc37f7d03b4f2fbd7492f67969f1225cf
Malvere <gatling1404@gmail.com>
2023-01-15 02:10:18 +0300

    Render.com Migration

--- envs.py
+++ envs.py
@@ -3,7 +3,7 @@ import os
 """ !Add PROJECT_NAME in heroku Vars! """
 API_TOKEN = os.getenv('API_TOKEN')
 PROJECT_NAME = os.getenv('PROJECT_NAME')
-WEBHOOK_HOST = f'https://{PROJECT_NAME}.herokuapp.com'  # Enter here your link from Heroku project settings
+WEBHOOK_HOST = f'https://{PROJECT_NAME}.onrender.com'  # Enter here your link from Heroku project settings
 WEBHOOK_URL_PATH = '/' + API_TOKEN
 WEBHOOK_URL = f'{WEBHOOK_HOST}{WEBHOOK_URL_PATH}'
 WEBAPP_HOST = os.getenv('localhost')
